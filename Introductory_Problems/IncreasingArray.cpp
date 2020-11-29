98b4ee02758c61a6ab0a9df53b3af6c35fdb0a9c
s3777242@rmit.edu.vn <s3777242@rmit.edu.vn>
2020-11-29 18:22:25 +0700

    Introductory

--- Introductory_Problems/IncreasingArray.cpp
+++ Introductory_Problems/IncreasingArray.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 using namespace std;
+#define ll long long
 
 int main(){
-    int n;
+    ll n;
     cin >> n;
-    
+    ll arr[n];
+    ll total = 0;
+    ll mx = 0;
+    for (ll i = 0 ; i < n; ++i) {
+        cin >> arr[i];
+        mx = max(arr[i], mx);
+        total += mx - arr[i];
+    }
+
+    cout << total;
 }
 
