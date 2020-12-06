ef19fec54ebb29e10c9475b5520cad89cafcdad6
s3777242@rmit.edu.vn <s3777242@rmit.edu.vn>
2020-12-06 16:40:08 +0700

    Introductory

--- Introductory_Problems/Bit_Strings.cpp
+++ Introductory_Problems/Bit_Strings.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 #define ll long long
-const ll mod = 1e9 +7;
+const ll mod = 1000000007;
+
+ll power(ll a, ll b, int m) {
+    ll r = 1, e = a;
+    while (b) {
+        if (b & 1) {
+            r = (r * e) % m;
+        }
+
+        e = (e * e) % m;
+        b >>= 1;
+    }
+    return r;
+}
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     ll n;
     cin >> n;
-    ll ans;
-    ans = pow(2,n);
-    ans = ans % mod;
-    cout << ans;
+
+    cout << power(2, n, mod);
 }
